3e6a49e7adb23eeb80e97e99b21f77a2317b8b58
Stephen Paulger <stephenpaulger@@users.noreply.github.com>
2021-10-30 10:10:51 +0100

    Add main() and remove global usage

--- download.py
+++ download.py
@@ -1,32 +1,32 @@
 import requests
 import os
+import sys
 from multiprocessing import Pool
+from functools import partial
 import time
 
-domain = input("Type the target domain: ")
 
-api_url = "https://web.archive.org/cdx/search/cdx?url=*."
-base_url = "http://web.archive.org/web/"
+API_URL = "https://web.archive.org/cdx/search/cdx?url=*."
+BASE_URL = "http://web.archive.org/web/"
 
-localDir = path = os.path.dirname(os.path.abspath(__file__))
-savePath = os.path.join(localDir, domain)
-os.mkdir(savePath)
 
-history = requests.get(api_url+domain).text.splitlines()
-
-waybackurls = []
-
-def getUrls(data):
+def getUrls(data, domain):
+    """
+    Return a set of wayback URLs
+    """
+    wayback_urls = set()
     for record in data:
         if "text/html" in record:
             items = record.split(' ')
             savedpage = items[0].split(')')[1]
-            url = domain+savedpage
+            url = domain + savedpage
             timestamp = items[1]
-            wayback_url = base_url+timestamp+"/"+url
-            waybackurls.append(wayback_url)
+            wayback_url = BASE_URL + timestamp + "/" + url
+            wayback_urls.add(wayback_url)
+    return wayback_urls
 
-def download(url):
+
+def download(savePath, url):
     noSlash = url.rstrip('/').replace('/', '£')
     if not noSlash.endswith('.txt'):
         output = os.path.join(savePath, noSlash)+".txt"
@@ -50,13 +50,33 @@ def download(url):
             print("Skipping url: ",url)
             break
         break
-    
-getUrls(history)
 
-print("Downloading {} pages".format(str(len(set(waybackurls)))))
-time.sleep(2)
 
-p = Pool(10)
-p.map(download, waybackurls)
-p.terminate()
-p.join()
+def main():
+    domain = input("Type the target domain: ")
+
+    localDir = path = os.path.dirname(os.path.abspath(__file__))
+    savePath = os.path.join(localDir, domain)
+
+    try:
+        os.mkdir(savePath)
+    except FileExistsError:
+        print("An output directory for the given domain already exists.")
+        print("Quitting to avoid over-writing your data")
+        sys.exit(1)
+
+    history = requests.get(API_URL + domain).text.splitlines()
+
+    waybackurls = getUrls(history, domain)
+
+    print("Downloading {} pages".format(str(len(waybackurls))))
+    time.sleep(2)
+
+    p = Pool(10)
+    p.map(partial(download, savePath), waybackurls)
+    p.terminate()
+    p.join()
+
+
+if __name__ == "__main__":
+    main()
